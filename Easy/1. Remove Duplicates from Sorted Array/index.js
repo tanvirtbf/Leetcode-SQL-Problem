2b2189f6c528cb5e13c2d288722d75bd1ddf4577
tanvirtbf <tanver018765@gmail.com>
2025-01-21 17:07:08 +0600

    start this repo

---
+++ Easy/1. Remove Duplicates from Sorted Array/index.js
@@ -0,0 +1,12 @@
+var removeDuplicates = function(nums) {
+  let uniqueIndex = 0;
+
+  for (let i = 1; i < nums.length; i++) {
+      if (nums[i] !== nums[uniqueIndex]) {
+          uniqueIndex++; 
+          nums[uniqueIndex] = nums[i]; 
+      }
+  }
+
+  return uniqueIndex + 1;
+};
