6d09d5050a28ab47cf987c815133b246902059fb
tanvirtbf <tanver018765@gmail.com>
2025-03-09 10:29:24 +0600

    40th Easy problem solve

--- Medium/7. Customers Who Bought All Products/index.sql
+++ Medium/7. Customers Who Bought All Products/index.sql
@@ -1,12 +1,7 @@
-WITH
-  AccumulatedQueue AS (
-    SELECT
-      person_name,
-      SUM(weight) OVER(ORDER BY turn) AS accumulated_weight
-    FROM Queue
-  )
-SELECT person_name
-FROM AccumulatedQueue
-WHERE accumulated_weight <= 1000
-ORDER BY accumulated_weight DESC
-LIMIT 1;
+SELECT customer_id
+FROM Purchase
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_id) = (
+    SELECT COUNT(*) 
+    FROM Product
+);
